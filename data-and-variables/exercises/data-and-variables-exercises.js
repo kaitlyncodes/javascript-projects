bb21c2425ed4b19ee69a4fa36e6156a6b93dbd9f
Kaitlyn Coppage <kaitlyncoppage@gmail.com>
2024-04-22 13:22:42 -0500

    days to moon

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,21 @@
 // Declare and assign the variables below
+let shuttleName = 'Determination';
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 38400;
+const milesPerKm = 0.621;
 
 // Use console.log to print the 'typeof' each variable. Print one item per line.
-
+console.log(typeof shuttleName);
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof milesPerKm);
 // Calculate a space mission below
-
+let milesToMoon = distanceToMarsKm * milesPerKm;
 // Print the results of the space mission calculations below
-
+let hoursToMoon = milesToMoon / shuttleSpeedMph;
 // Calculate a trip to the moon below
-
+let daysToMoon = hoursToMoon / 24;
 // Print the results of the trip to the moon below
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon.");
